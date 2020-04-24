d97a496b0d6b6c795ea894d296e80ad7b7e1e8a3
vitwit <sunilv@vitwit.com>
2020-04-24 08:31:25 +0530

    Custom VPC

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "profile"{
+    type = string
+    default = "profile2"
+}
+
+variable "region"{
+    type = string
+    default = "us-east-1"
+}
+
+variable "env"{
+    description = "env: dev or prod"
+}
+
+
+variable "vpc_name" {
+  type        = map
+  description = "Name for vpc"
+}
+variable "vpc_cidr"{
+    type = map
+}
+
+variable "public_cidrs"{
+    type = map
+}
+
+variable "az"{
+    type=map
+}
+
+variable "web_cidrs"{
+    type = map
+}
+
+variable "app_cidrs"{
+     type = map
+}
